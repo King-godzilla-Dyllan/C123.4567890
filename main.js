b16461a57275b781e7e3af160993ab488c5cd0c3
King-godzilla-Dyllan <132647334+King-godzilla-Dyllan@users.noreply.github.com>
2023-10-14 20:28:09 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function setup() {
+
+    canvas = createCanvas(550, 549.99)
+    canvas.position(560,150)
+
     video = createCapture(VIDEO);
     video.size(550, 500);
 
-    canvas = createCanvas(550, 349.99)
-    canvas.position(560,150)
+    
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses)
